b8ca71026003c51c28c6c548c5b02d66a34aa83d
Ayush Chaurasiya <147719677+AyushChaursiaya@users.noreply.github.com>
2024-08-09 10:57:20 +0000

    codeall

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -3,7 +3,7 @@ import React from "react";
 
 const Welcome = ({ name }) => {
     return (<div>
-        <h1>`this {name}!`</h1>
+        <h1>{`Hey ${name}!`}</h1>
         <h2>'Welcome to School.'</h2>
     </div>)
 }
