b8c435237b0a5cc79b157ca55eb2685a9913c030
Ayush Chaurasiya <147719677+AyushChaursiaya@users.noreply.github.com>
2024-08-09 10:43:52 +0000

    codefile

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,8 +1,11 @@
 // write code for Welcome component here
 import React from "react";
 
-const Welcome = ({name}) => {
-    return <h2>`this {name}!`</h2>
+const Welcome = ({ name }) => {
+    return (<>
+        <h1>`this {name}!`</h1>
+        <h2>"Welcome to School."</h2>
+    </>)
 }
 
 export default Welcome;
